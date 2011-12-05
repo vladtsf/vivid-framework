e92cb9ce2668e21633b800a62fa4a6fb6cce6a29
Vladimir Tsvang <vtsvang@gmail.com>
2011-12-05 11:39:56 +0300

    Template must be loaded before the Control

--- bin/vivid.js
+++ bin/vivid.js
@@ -237,6 +237,105 @@ Vivid.Cookie = (function() {
 	    } 
     };
 })();
+Vivid.Template = (function() {
+    
+    /**
+     * Variable search RegExp
+     */
+    var variable = (/\:([\w])*\:/g);
+
+    /**
+     * Quotes replacing template
+     */
+    var quotes = (/:/g);
+    
+    /**
+     * Simple template engine
+     * 
+     * @name $.Vivid.Template
+     * @param {string} markup template markup
+     * @example
+     *	var tpl = new Template('Hello, :entityname:!');
+     *	tpl.compile();
+     *	alert(tpl.eval({entityname: 'world'})); // Hello, world!
+     */
+    var Template = function(markup) {
+	if(this instanceof Template) {
+
+	    /**
+	     * Evaluated code
+	     * 
+	     * @private
+	     */
+	    var code = '';
+
+	    /**
+	     * Replaces the variable to executable code fragement
+	     * 
+	     * @param {string} match current match
+	     * @private
+	     */
+	    var parse = function(match) {
+		if(match == '::') {
+		    return match;
+		}
+		
+		var
+			varName = match.replace(quotes, '');
+
+		return '"+(function(){try{return ' + varName + ';}catch(__e){return "";}})()+"';
+	    }
+
+	    /**
+	     * Compiles template markup to executable js code
+	     * 
+	     * @return Template
+	     */
+	    this.compile = function() {
+		code = ('"' + markup.replace(/"/g, '\\"').replace(variable, parse) + '"');
+		return this;
+	    };
+
+	    /**
+	     * Substitutes values ​​of variables in the template
+	     * 
+	     * @param {object} variables template variables
+	     * @param {function} callback the asynchronous call callback
+	     */
+	    this.eval = function(variables, callback) {
+		var
+		    result,
+		    vars = variables || new Object();
+
+		if(typeof(callback) == 'function') {
+		    setTimeout(function() {
+			with(vars) {
+			    result = eval(code);
+			}
+
+			callback.call(this, result);
+		    }, 0);
+
+		    return this;
+		}	
+
+		with(vars) {
+		    result = eval(code);
+		}
+
+		return result;
+	    };
+
+	    return this;
+	}
+
+	return new Template(markup);
+    };
+
+    return Template;
+})();
+
+
 Vivid.Control = (function() {
     
     /**
@@ -395,105 +494,6 @@ Vivid.Control = (function() {
     
     return Control;
 })();
-Vivid.Template = (function() {
-    
-    /**
-     * Variable search RegExp
-     */
-    var variable = (/\:([\w])*\:/g);
-
-    /**
-     * Quotes replacing template
-     */
-    var quotes = (/:/g);
-    
-    /**
-     * Simple template engine
-     * 
-     * @name $.Vivid.Template
-     * @param {string} markup template markup
-     * @example
-     *	var tpl = new Template('Hello, :entityname:!');
-     *	tpl.compile();
-     *	alert(tpl.eval({entityname: 'world'})); // Hello, world!
-     */
-    var Template = function(markup) {
-	if(this instanceof Template) {
-
-	    /**
-	     * Evaluated code
-	     * 
-	     * @private
-	     */
-	    var code = '';
-
-	    /**
-	     * Replaces the variable to executable code fragement
-	     * 
-	     * @param {string} match current match
-	     * @private
-	     */
-	    var parse = function(match) {
-		if(match == '::') {
-		    return match;
-		}
-		
-		var
-			varName = match.replace(quotes, '');
-
-		return '"+(function(){try{return ' + varName + ';}catch(__e){return "";}})()+"';
-	    }
-
-	    /**
-	     * Compiles template markup to executable js code
-	     * 
-	     * @return Template
-	     */
-	    this.compile = function() {
-		code = ('"' + markup.replace(/"/g, '\\"').replace(variable, parse) + '"');
-		return this;
-	    };
-
-	    /**
-	     * Substitutes values ​​of variables in the template
-	     * 
-	     * @param {object} variables template variables
-	     * @param {function} callback the asynchronous call callback
-	     */
-	    this.eval = function(variables, callback) {
-		var
-		    result,
-		    vars = variables || new Object();
-
-		if(typeof(callback) == 'function') {
-		    setTimeout(function() {
-			with(vars) {
-			    result = eval(code);
-			}
-
-			callback.call(this, result);
-		    }, 0);
-
-		    return this;
-		}	
-
-		with(vars) {
-		    result = eval(code);
-		}
-
-		return result;
-	    };
-
-	    return this;
-	}
-
-	return new Template(markup);
-    };
-
-    return Template;
-})();
-
-
 
 $.fn.log = function() {
 	Vivid.Console.log(this);
--- build/overall.tpl.js
+++ build/overall.tpl.js
@@ -5,8 +5,8 @@
 //<!include file="../src/console.js"-->
 //<!include file="../src/collection.js"-->
 //<!include file="../src/cookie.js"-->
-//<!include file="../src/control.js"-->
 //<!include file="../src/template.js"-->
+//<!include file="../src/control.js"-->
 
 //<!include file="../src/outro.js"-->
 	
